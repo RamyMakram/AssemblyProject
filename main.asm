e6d3b9c9607402322b52aea0f7b5fb57d11dbc3a
RamyMakram <ramymakrameyd@outlook.com>
2019-12-13 18:05:45 +0200

    Finsh

--- main.asm
+++ main.asm
@@ -24,7 +24,7 @@ EnterEq byte "Enter Equation To Solve: ",0
 Equation byte 35 dup(-1)
 Co dword 35 dup(-1)
 Power dword 35 dup(-1)
-Sign dword 35 dup(?)
+Return dword 35 dup(-1)
 Val byte 0
 PowerFlag byte 0
 LenghtOfEnterd dword 0
@@ -32,16 +32,26 @@ temp dword 0
 ReadedX dword 0
 NumOfTerms dword 0
 offsetOfEq dword 0
-;#########################Q1 DATA##############################	
+OutPut byte 35 dup(?),0
 																					
 ;#########################Q2 DATA##############################	
 
-;#########################Q2 DATA##############################		
-
+TragEquation byte 35 dup(-1)
+SizeOFTrag dword 0
+Sin byte "Cos(",0
+Cos byte "-Sin(",0
+Tan byte "Sec^2(",0
+EQSize dword 0
+ESITemp dword 0
+ECXTemp dword 0
 ;#########################Q3 DATA##############################	
+EqDev byte 35 dup(-1)
+IsMakam dword 0
 bast byte 35 dup(-1)
 makam byte 35 dup(-1)
-;#########################Q3 DATA##############################		
+NString dword 0
+NBast dword 0
+NMakam dword 0
 													
 														
 														
@@ -49,7 +59,7 @@ makam byte 35 dup(-1)
 														
 ;#######################################################
 MAIN PROC									  ;#
-mov edx,offset intro								  ;#
+		mov edx,offset intro						  ;#
 		call writestring						      ;#
 		call crlf									  ;#
 		call crlf									  ;#
@@ -83,12 +93,17 @@ mov edx,offset intro								  ;#
 	PROGLOOP:
 		mov esi,0
 		mov ecx,35
+		mov edi,0
 		loopp:
 			mov Co[esi],-1
 			mov Power[esi],-1
-			mov Sign[esi],-1
+			mov Return[esi],-1
+			mov EqDev[edi],-1
+			mov makam[edi],-1
+			mov bast[edi],-1
 			add esi,4
-		loop loopp												  ;#
+			inc edi
+		loop loopp									  ;#
 		call crlf									  ;#
 		MOV EDX, OFFSET PRMPT						  ;#
 		CALL WRITESTRING							  ;#
@@ -142,10 +157,6 @@ MAIN ENDP											  ;#
 ;----------------------------------------------------------
 Q1 PROC
 
-
-
-
-
 	mov NumOfTerms,0
 	inc eax
 	mov LenghtOfEnterd,eax
@@ -154,7 +165,6 @@ Q1 PROC
 	mov ebx,0 ;total Calc
 	mov esi, offset Co
 	mov edi,offset Power
-	mov ebp,offset Sign
 	mov PowerFlag,0 ;flag of power
 	looop:
 
@@ -163,12 +173,14 @@ Q1 PROC
 		je skip
 		cmp eax,43 ; value is +
 		je skip
+		cmp eax,-1 ; value is +
+		je Out2
 		cmp ecx,LenghtOfEnterd
-		je AddFirstPlus
+		je Check_FirstX
 		jmp Action
 
-	 AddFirstPlus:
-		call AddFirstPlusProc
+	Check_FirstX:
+		call CheckFirst_X
 
 	Action:
 		movzx eax, Equation[edx]
@@ -195,11 +207,9 @@ Q1 PROC
 		jmp assignCof
 
 	skip:
-		mov [ebp],eax
-		add ebp,4
-		call CheckFreeVar
 		cmp PowerFlag,1 ;check power flag
 		je assignPower
+		call CheckFreeVar
 		jmp continue
 
 	assignCof:
@@ -209,6 +219,7 @@ Q1 PROC
 
 	assignPower:
 		call AssignPowerProc
+		call CheckFreeVar
 
 	continue:
 		inc edx
@@ -216,7 +227,8 @@ Q1 PROC
 		cmp ecx,0
 		jne looop    
 
-
+Out2:
+mov ecx,0
 	;###########################Der##################################
 	mov esi, offset Co
 	mov edi,offset Power
@@ -243,8 +255,8 @@ Q1 PROC
 	;###########################Print##################################
 	mov esi, offset Co
 	mov edi,offset Power
-	mov ebx,offset Sign
 	mov ecx,NumOfTerms
+	mov ebp,0
 	loop3:
 
 		cmp ecx,NumOfTerms
@@ -255,30 +267,21 @@ Q1 PROC
 		mov eax,[esi]
 		cmp eax,0
 		je cont
-		mov edx,45 
-		cmp [ebx],edx ;cmp mince
-		je PrintMince
-		add ebx,4
-		jmp Actions2
-
-	PrintMince:
-		mov al,'-'
-		call writechar
+		call writedec
+		call AsignInReturn
 		jmp Actions2
 
 	PrintSign:
 		mov eax,[esi]
 		cmp eax,0
 		je cont
-		mov al,[ebx]
-		call writechar
-		add ebx,4
+		call writeint
+		mov eax,43
+		call AsignInReturn
+		mov eax,[esi]
+		call AsignInReturn
 
 	Actions2:
-		mov eax,[esi]
-		cmp eax,0
-		je cont
-		call writedec
 		mov eax,[edi]
 		cmp eax,0
 		jle cont ;print only Co
@@ -289,22 +292,29 @@ Q1 PROC
 	PrintX:
 		mov al,'X'
 		call writechar
+		mov eax,88
+		call AsignInReturn
 		jmp cont
 
 	PrintAll:
 		mov al,'X'
 		call writechar
+		mov eax,88
+		call AsignInReturn
 		mov al,'^'
 		call writechar
+		mov eax,94
+		call AsignInReturn
 		mov eax,[edi]
 		call writedec
 
 	cont:
 		add esi,4
 		add edi,4
-
-	loop loop3
-
+		dec ecx
+		cmp ecx,0
+		jne loop3
+RET
 ;###########################CalcAppend##################################
 CalcAppend PROC
 	movzx eax, Equation[edx]
@@ -345,10 +355,7 @@ continue:
 		RET
 AssignPowerProc ENDP
 
-AddFirstPlusProc PROC
-	mov eax,43
-	mov [ebp],eax
-	add ebp,4
+CheckFirst_X PROC
 	movzx eax, Equation[edx]
 	CMP EAX,88 ;THERE X
 	JE FirstIsX
@@ -360,7 +367,7 @@ FirstIsX:
 Cont:
 	movzx eax, Equation[edx]
 RET
-AddFirstPlusProc ENDP
+CheckFirst_X ENDP
 
 CheckFreeVar PROC
 	movzx eax, Equation[edx+1]
@@ -376,28 +383,357 @@ Cont:
 RET
 CheckFreeVar ENDP	
 
-
-
-
-call crlf
+AsignInReturn PROC
+	mov Return[ebp],eax
+	add ebp,4
 RET
+AsignInReturn ENDP
+
+PrepairToPrint PROC
+	mov edx, 0             ; dividend high half = 0.  prefer  xor edx,edx
+	mov ebx, 10            ; divisor can be any register or memory
+	div ebx ; Divides 1234 by 10.
+			; EDX =   4 = 1234 % 10  quotient
+			; EAX = 123 = 1234 / 10  remainder
+RET
+PrepairToPrint ENDP
+
 Q1 ENDP
 
 
 ;----------------------------------------------------------
 ;----------------------------------------------------------
 Q2 PROC
-Call Panic
+mov ecx,35
+mov edx,offset TragEquation
+call readstring
+mov ecx,eax
+mov SizeOFTrag,eax
+mov esi,0
+LoopQQQ1:
+
+mov al,TragEquation[esi]
+cmp al,83
+je Sin_Lable
+cmp al,67
+je Cos_Lable
+cmp al,84
+je Tan_Lable
+cmp al,')'
+je ContQ3_
+cmp al,'+'
+je ContQ3_
+cmp al,0
+je ContQ3_
+
+Sin_Lable:
+	add esi,4
+	sub ecx,4
+	mov ESITemp,esi
+	mov ECXTemp,ecx
+	call SinProc
+	mov eax,EQSize
+	call Q1
+	mov esi,ESITemp
+	mov ecx,ECXTemp
+	jmp ContQ3_
+Cos_Lable:
+	add esi,4
+	sub ecx,4
+	mov ESITemp,esi
+	mov ECXTemp,ecx
+	call CosProc
+	mov eax,EQSize
+	call Q1
+	mov esi,ESITemp
+	mov ecx,ECXTemp
+	jmp ContQ3_
+Tan_Lable:
+	add esi,4
+	sub ecx,4
+	mov ESITemp,esi
+	mov ECXTemp,ecx
+	call TanProc
+	mov eax,EQSize
+	call Q1
+	mov esi,ESITemp
+	mov ecx,ECXTemp
+	jmp ContQ3_
+
+ContQ3_:
+	add eax,0
+	inc esi
+	dec ecx
+	cmp ecx,0
+	jne LoopQQQ1 
+
+
+
+
+
 call crlf
 		RET
-Q2 ENDP 
 
+;########################################################################
+SinProc PROC
+mov eax,SizeOFTrag
+sub eax,4
+cmp ecx,eax
+jne PrintSignTrag1
+jmp Outq1
+
+PrintSignTrag1:
+	mov al,'+'
+	call writechar
+
+Outq1:
+mov EQSize,0
+mov edx,offset Sin
+call writestring
+mov ESITemp,esi
+call EmptyArrays
+mov esi,ESITemp
+mov edi,offset Equation
+mov ecx,ECXTemp
+lopp1:
+	mov al,TragEquation[esi]
+	cmp al,')'
+	je CaallQ1_1
+	call writechar
+	mov [edi],al
+	inc EQSize
+	inc esi
+	inc edi
+	dec ECXTemp
+loop lopp1
+
+CaallQ1_1:
+	mov al,')'
+	call writechar
+	mov al,'*'
+	call writechar
+	mov eax,EQSize
+	mov ESITemp,esi
+	mov ECXTemp,ecx
+RET
+SinProc ENDP
+
+;#######################################################################
+CosProc PROC
+mov EQSize,0
+mov edx,offset Cos
+call writestring
+mov ESITemp,esi
+call EmptyArrays
+mov esi,ESITemp
+mov edi,offset Equation
+mov ecx,ECXTemp
+lopp2:
+	mov al,TragEquation[esi]
+	cmp al,')'
+	je CaallQ1_2
+	call writechar
+	mov [edi],al
+	inc EQSize
+	inc esi
+	inc edi
+	dec ECXTemp
+loop lopp2
+
+CaallQ1_2:
+	mov al,')'
+	call writechar
+	mov al,'*'
+	call writechar
+	mov eax,EQSize
+	mov ESITemp,esi
+	mov ECXTemp,ecx
+RET
+CosProc ENDP
+;################################################################################3
+TanProc PROC
+mov eax,SizeOFTrag
+sub eax,4
+cmp ecx,eax
+jne PrintSignTrag3
+jmp Outq
+
+PrintSignTrag3:
+	mov al,'+'
+	call writechar
+
+Outq:
+mov EQSize,0
+mov edx,offset Tan
+call writestring
+mov ESITemp,esi
+call EmptyArrays
+mov esi,ESITemp
+mov edi,offset Equation
+mov ecx,ECXTemp
+lopp3:
+	mov al,TragEquation[esi]
+	cmp al,')'
+	je CaallQ1_3
+	call writechar
+	mov [edi],al
+	inc EQSize
+	inc esi
+	inc edi
+	dec ECXTemp
+loop lopp3
+
+CaallQ1_3:
+	mov al,')'
+	call writechar
+	mov al,'*'
+	call writechar
+	mov eax,EQSize
+	mov ESITemp,esi
+	mov ECXTemp,ecx
+RET
+TanProc ENDP
+
+
+Q2 ENDP 
 ;----------------------------------------------------------
 ;----------------------------------------------------------
 
 Q3 PROC
-		
+mov ecx,35
+mov edx, offset EqDev
+call readstring
+mov ecx, eax 
+mov esi,0
+mov edi,offset bast
+mov ebp,offset makam
+mov NBast,0
+mov NMakam,0
+mov IsMakam,0
+
+loopQQ:
+movzx eax,EqDev[esi]
+cmp IsMakam,1
+je AssignMakam
+cmp eax,47
+je IsMakamLable
+mov [edi],al
+inc NBast
+inc edi
+jmp Continiutesss
+
+IsMakamLable:
+	mov IsMakam,1
+	jmp Continiutesss
+
+AssignMakam:
+	mov [ebp],al
+	inc NMakam
+	inc ebp
+
+Continiutesss:
+	inc esi
+
+loop loopQQ
+call EmptyArrays
+mov ecx,NBast
+mov esi, offset Equation
+mov edi, offset bast
+loop1QQ:
+	mov al,[edi]
+	mov [esi],al
+	inc edi
+	inc esi
+loop loop1QQ
+mov al,'['
+call writechar
+mov al,'('
+call writechar
+mov eax,NBast
+call Q1
+mov al,')'
+call writechar
+mov al,'*'
+call writechar
+mov al,'('
+call writechar
+mov edi,offset makam
+mov ecx,NMakam
+loop2QQ:
+	mov al,[edi]
+	call writechar
+	inc edi
+loop loop2QQ
+mov al,')'
+call writechar
+mov al,'-'
+call writechar
+mov al,'('
+call writechar
+
+call EmptyArrays
+mov ecx,NMakam
+mov esi, offset Equation
+mov edi, offset makam
+loop3QQ:
+	mov al,[edi]
+	mov [esi],al
+	inc edi
+	inc esi
+loop loop3QQ
+mov eax,NMakam
+call Q1
+mov al,')'
+call writechar
+mov al,'*'
+call writechar
+mov al,'('
+call writechar
+mov edi,offset bast
+mov ecx,NBast
+loop21QQ:
+	mov al,[edi]
+	call writechar
+	inc edi
+loop loop21QQ
+mov al,')'
+call writechar
+mov al,']'
+call writechar
+mov al,'/'
+call writechar
+mov al,'('
+call writechar
+mov edi,offset makam
+mov ecx,NMakam
+loop5QQ:
+	mov al,[edi]
+	call writechar
+	inc edi
+loop loop5QQ
+mov al,')'
+call writechar
+mov al,'^'
+call writechar
+mov al,'2'
+call writechar
+
 Q3 ENDP
+
+EmptyArrays PROC
+		mov esi,0
+		mov edi,0
+		mov ecx,35
+		loopp:
+			mov Co[esi],-1
+			mov Equation[edi],-1
+			mov Power[esi],-1
+			mov Return[esi],-1
+			add esi,4
+			inc edi
+		loop loopp	
+RET
+EmptyArrays ENDP
 
 ;----------------------------------------------------------
 ;----------------------------------------------------------
